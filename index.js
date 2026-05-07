263601b6eb8c806e7c92e2435c449e30f8903db3
shanto2575 <shanto2575@gmail.com>
2026-05-07 11:10:14 +0600

    delete action

--- index.js
+++ index.js
@@ -38,6 +38,15 @@ async function run() {
             console.log('userid', id)
             res.send(users)
         })
+        
+        app.delete('/users/:id',async(req,res)=>{
+            const id=req.params.id;
+            const query={
+                _id: new ObjectId(id)
+            }
+            const result=await userCollection.deleteOne(query)
+            res.send(result)
+        })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
